213da6f6112f8613b8e2ef906ace8aedf188dbe8
Muhammet Akduman <akdumanmxc@gmail.com>
2026-08-23 22:45:04 +0300

    Biz Sale state'ini değiştirirken satırı transaction boyunca kililenicek Lock anotasyonu

--- sales-service/src/main/java/com/muhammet/sales_service/sale/repository/SaleRepository.java
+++ sales-service/src/main/java/com/muhammet/sales_service/sale/repository/SaleRepository.java
@@ -1,8 +1,25 @@
 package com.muhammet.sales_service.sale.repository;
 
 import com.muhammet.sales_service.sale.domain.Sale;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface SaleRepository extends JpaRepository<Sale, Long> {
+import java.util.Optional;
 
+public interface SaleRepository
+        extends JpaRepository<Sale, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+            SELECT s
+            FROM Sale s
+            WHERE s.id = :saleId
+            """)
+    Optional<Sale> findByIdForUpdate(
+            @Param("saleId")
+            Long saleId
+    );
 }
