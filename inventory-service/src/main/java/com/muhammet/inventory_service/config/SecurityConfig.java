60465939a4db0249013dd6a41a0ed040648cd50d
Muhammet Akduman <akdumanmxc@gmail.com>
2026-08-25 21:09:02 +0300

    Swagger public , busines enpointler jwtli

--- inventory-service/src/main/java/com/muhammet/inventory_service/config/SecurityConfig.java
+++ inventory-service/src/main/java/com/muhammet/inventory_service/config/SecurityConfig.java
@@ -12,17 +12,48 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
         http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/actuator/**").permitAll()
-                .anyRequest().authenticated())
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> {}));
+                .csrf(csrf ->
+                        csrf.disable()
+                )
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
+                .authorizeHttpRequests(auth ->
+                        auth
+
+                                // Actuator
+                                .requestMatchers(
+                                        "/actuator/**"
+                                )
+                                .permitAll()
+
+                                // Swagger / OpenAPI
+                                .requestMatchers(
+                                        "/swagger-ui.html",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**"
+                                )
+                                .permitAll()
+
+                                // Business endpoints
+                                .anyRequest()
+                                .authenticated()
+                )
+
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> {
+                        })
+                );
+
         return http.build();
     }
 }
-
